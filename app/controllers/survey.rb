2cba73a16fd2e03bb672dbcdada626ec69167e78
Apprentice <apprentice@master-inf-11.local>
2014-09-04 21:10:54 -0700

    Fucked some shit up

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,35 +1,32 @@
-get '/users/:id' do
-  @user = User.find(params[:id])
-  erb :profile
-end
-
-
-#render specific survey with partial
-get '/users/:id/surveys/:survey_id' do
-
-  erb :_survey
-end
-
 #create button on profile redirects to creation form
 get '/users/:id/surveys/new' do
-  @new_survey = Survey.create(user_id: params[:id])
+  @user = User.find(params[:id])
   erb :create
 end
 
-
-############ SURVEY CREATION #############
-
-# #add question
-# post '/users/:id/surveys/:survey_id/questions/new' do
-
-# end
-
-# #add choice to question
-# post '/users/:id/surveys/:survey_id/questions/:question_id/choices/new' do
-
-# end
-
-#submit completed survey, redirect to profile.erb
-post '/users/:id/surveys' do
-  # current_user.surveys << survey creation
+#render specific survey with partial
+get '/users/:id/surveys/:survey_id' do
+  erb :_survey, layout: false
 end
+
+post '/users/:id/surveys' do
+  survey = Survey.create(title: params[:title])
+  current_user.surveys << survey
+  erb :_question, layout: false, locals: {survey: survey}
+end
+
+post '/users/:id/surveys/:survey_id/questions' do
+  survey = Survey.find(params[:survey_id])
+  question = Question.create(content: params[:question])
+  survey.questions << question
+  erb :_choice, layout: false, locals: {survey: survey, question: question}
+end
+
+# post '/users/:id/surveys/:survey_id/questions/:question_id/choices' do
+#   survey = Survey.find(params[:survey_id])
+#   question = Question.find(params[:question_id])
+#   choice = question.choices.create(content: params[:content])
+#   erb :_choice, layout: false, locals: {survey: survey, question: question, choice: choice}
+# end
+
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,84 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+//  Our attempt at dynamic question/choice
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+// $(document).ready(function() {
+//   $('.add_q').on("click", addQuestion);
+// });
+
+// var questionCount = 0;
+// var choiceCount = 0;
+
+// function addQuestion(e){
+//   e.preventDefault();
+
+//   questionCount++;
+
+//   $('.newsurvey').append('<form action="/create/new" method="post" class="question' + questionCount +'"><br><input  type="text" name="question' + questionCount + '" placeholder="enter a question"/><br>');
+//   $('.newsurvey').append('<button class="add_c' + questionCount + '"> ADD CHOICE </button> <input class="next" type="submit" value="Done" action="/create/"></form>');
+
+//   $('.add_c'+ questionCount).on("click", addChoice);
+
+// }
+
+
+
+// function addChoice(e){
+//   choiceCount++
+//   e.preventDefault();
+//   $('.question' + questionCount).append('<input class="choice" type="text" name="choice'+ choiceCount + '" placeholder="Enter a choice"/><br>');
+// }
+
+//  ------------------------------------
+
+$(document).ready(function() {
+  $('.survey').on('click', function(event) {
+    event.preventDefault();
+    var link = $(this);
+    $.ajax({
+      url: link.attr('href'),
+      type: "GET"
+    }).done(function(server_data){
+      console.log(server_data)
+      $('#survey').append(server_data);
+    })
+  }),
+
+  $('#newsurvey').on('submit', function(event){
+    event.preventDefault();
+    serialize = $(this).serialize(),
+    $.ajax({
+      url: $(this).attr("action"),
+      type: "post",
+      data: serialize
+    }).done(function(server_data) {
+      $('.survey_container').append(server_data)
+
+      $('.questions').on('submit', function(event) {
+        console.log($(this).serialize())
+        event.preventDefault();
+        $.ajax({
+          url: $(this).attr('action'),
+          type: "post",
+          data: $(this).serialize
+        }).done(function(s_data) {
+          $('.survey_container').append(s_data);
+        })
+      })
+    })
+  })
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
